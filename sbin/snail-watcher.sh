905823610db29e7920c17452dae39c08aa96f898
Kolan Sh <mecareful@gmail.com>
2012-05-15 04:29:29 +0400

    Stability improved. Latency reduced. Also fixes #76.

--- sbin/snail-watcher.sh
+++ sbin/snail-watcher.sh
@@ -29,10 +29,12 @@ rm -f $FIFO_FILE && mkfifo --mode 660 $FIFO_FILE && chgrp video $FIFO_FILE # cre
 
   while [ 1 ]; do
     read <$FIFO_FILE
+    echo >$FIFO_FILE | cat $FIFO_FILE >/dev/null # flush fifo
     snail.nv_pwr_on && $XSERVER_START_CMD # starting X server
     flock -u 9
     echo >$FIFO_FILE | cat $FIFO_FILE >/dev/null # flush fifo
     flock 9
+    echo >$FIFO_FILE | cat $FIFO_FILE >/dev/null # flush fifo
     $XSERVER_STOP_CMD && snail.nv_pwr_off # stopping X server
   done
 } 9>$LOCK_FILE
